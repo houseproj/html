5580c2e594bed990701d31792a81150a223b8b6a
Vlad <wladiwate@gmail.com>
2019-03-27 14:07:40 +0300

    Первый комит

---
+++ script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+
+// console.log(second);
+
+// alert('hello word');
+
+// let ans = confirm('Как дела?');
+// console.log(ans);
+
+// let ans = prompt('Ваш возраст:', '18');
+// console.log(typeof(ans));
+
+
+// console.log("arr" + "obj");
+// console.log(4 + +"obj");
+
+
+// let incr = 10, 
+//     decr = 10;
+
+//     incr++;
+//     decr--;
+
+//     console.log(incr);
+//     console.log(decr);
+
+    console.log(5%2);
+    console.log("2" == 2);
